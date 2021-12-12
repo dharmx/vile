d75e8a18c3a113d6f5a93801865a9f2fcc427ad3
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2021-12-12 16:01:40 +0530

    added vile (eww) config support

---
+++ colors.sh
@@ -0,0 +1,18 @@
+# Colors
+color0='#2e3440'
+color8='#434C5E'
+color1='#BF616A'
+color9='#D08770'
+color2='#A3BE8C'
+color10='#A3BE8C'
+color3='#EBCB8B'
+color11='#EBCB8B'
+color4='#5E81AC'
+color12='#81A1C1'
+color5='#B48EAD'
+color13='#B48EAD'
+color6='#88C0D0'
+color14='#8FBCBB'
+color7='#d8dee9'
+color15='#ECEFF4'
+
