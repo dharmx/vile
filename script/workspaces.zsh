7009e5885c79fab3fc484b3cc9270c4926c992ff
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2022-05-03 21:14:53 +0530

    minor adjustments

--- script/workspaces.zsh
+++ script/workspaces.zsh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
-function make_button() {
+function _make_button() {
   print "(button :class 'vertigo-button vertigo-workspace vertigo-workspace-"$2"' :tooltip 'workspace: "$1" state: "$2"' :onclick 'bspc desktop --focus "$3"' '"$1"')"
 }
 
-function wrap_yuck() {
+function _wrap_yuck() {
   local buffered=""
-  alias query="bspc query --desktops --desktop"
+  alias query="bspc query --names --desktops --desktop"
   # newlined str -> spaced str -> array, eg: "1\n2\n3" -> "1 2 3" -> (1 2 3)
   local _focused=(${$(query .focused)//$'\n'/ })
   local _urgent=(${$(query .urgent)//$'\n'/ })
@@ -28,16 +28,38 @@ function wrap_yuck() {
     else
       _local[$index]=$index && state="local"
     fi
-    buffered+=$(make_button $_local[$index] $state $index)
+    buffered+=$(_make_button $_local[$index] $state $index)
   done
   print $buffered
   unalias query
 }
 
-function make_box() {
-  print "(box :orientation 'vertical' :class 'vertigo-box vertigo-desktop' :space-evenly true :tooltip 'workspaces' "$(wrap_yuck)")"
+function _make_box() {
+  print "(box :orientation 'vertical' :class 'vertigo-box vertigo-desktop' :space-evenly true :tooltip 'workspaces' "$(_wrap_yuck)")"
 }
 
-make_box && bspc subscribe desktop node | while read -r _ do; make_box done
+function subscribe() {
+  _make_box && bspc subscribe desktop node | while read -r _ do; _make_box done
+}
+
+function create() {
+  local workspaces=(${$(bspc query --names --desktops --desktop .local)//$'\n'/ })
+  workspaces+=$((${#workspaces} + 1))
+  bspc monitor -d $workspaces[*]
+}
+
+function remove() {
+  local workspaces=(${$(bspc query --names --desktops --desktop .local)//$'\n'/ })
+  unset 'workspaces[-1]'
+  bspc monitor -d $workspaces[*]
+}
+
+case $1 in
+  subscribe) subscribe ;;
+  create) create ;;
+  remove) remove ;;
+  *) print "Invalid option!" ;;
+esac
+
 
 # vim:ft=zsh
