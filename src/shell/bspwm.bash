760c2e148e079437ce107cd77b280e9738a0cd76
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2022-07-24 02:31:55 +0530

    chore: documented shell scripts

--- src/shell/bspwm.bash
+++ src/shell/bspwm.bash
@@ -1,52 +1,76 @@
 #!/usr/bin/env bash
 # shellcheck disable=2154,1091
 
-source "$XDG_CONFIG_HOME/bspwm/theme.bash"
+# Authored By pagankeymaster <pagankeymaster@gmail.com> under:
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+#
+# Permissions of this strong copyleft license are conditioned on
+# making available complete source code of licensed works and
+# modifications, which include larger works using a licensed work,
+# under the same license. Copyright and license notices must be
+# preserved. Contributors provide an express grant of patent rights.
+#
+# Read the complete license here:
+# <https://github.com/pagankeymaster/vile/blob/main/LICENSE.txt>
 
+# Gist
+# Script that checks and notifies if a 'bspc config' option has been changed.
+
+source "$XDG_CONFIG_HOME/bspwm/theme.bash" # base16 colors
+
+# Not really going to explain what these mean 
+# instead use read the bspc manpage
 wm_config=(
-	border_width
-	window_gap
-	top_padding
-	normal_border_color
-	focused_border_color
-	active_border_color
-	presel_feedback_color
-	split_ratio
-	mapping_events_count
-	borderless_monocle
-	gapless_monocle
-	borderless_singleton
-	single_monocle
-	center_pseudo_tiled
-	presel_feedback
-	honor_size_hints
-	remove_disabled_monitors
-	removal_adjustment
-	remove_unplugged_monitors
-	merge_overlapping_monitors
-	automatic_scheme
-	initial_polarity
-	directional_focus_tightness
-	top_monocle_padding
-	right_monocle_padding
-	bottom_monocle_padding
-	left_monocle_padding
-	pointer_motion_interval
-	pointer_modifier
-	pointer_action1
-	pointer_action2
-	pointer_action3
-	click_to_focus
-	swallow_first_click
-	focus_follows_pointer
-	pointer_follows_focus
-	pointer_follows_monitor
-	ignore_ewmh_focus
-	ignore_ewmh_fullscreen
-	ignore_ewmh_struts
+  border_width
+  window_gap
+  top_padding
+  normal_border_color
+  focused_border_color
+  active_border_color
+  presel_feedback_color
+  split_ratio
+  mapping_events_count
+  borderless_monocle
+  gapless_monocle
+  borderless_singleton
+  single_monocle
+  center_pseudo_tiled
+  presel_feedback
+  honor_size_hints
+  remove_disabled_monitors
+  removal_adjustment
+  remove_unplugged_monitors
+  merge_overlapping_monitors
+  automatic_scheme
+  initial_polarity
+  directional_focus_tightness
+  top_monocle_padding
+  right_monocle_padding
+  bottom_monocle_padding
+  left_monocle_padding
+  pointer_motion_interval
+  pointer_modifier
+  pointer_action1
+  pointer_action2
+  pointer_action3
+  click_to_focus
+  swallow_first_click
+  focus_follows_pointer
+  pointer_follows_focus
+  pointer_follows_monitor
+  ignore_ewmh_focus
+  ignore_ewmh_fullscreen
+  ignore_ewmh_struts
 )
 
 function get_json() {
+  # packs the current bspc config values into a json format
+  # jq is a bit slow if you are wondering.
   local primed="{"
   for option in "${wm_config[@]}"; do
     primed+="\"$option\":\"$(bspc config "$option")\","
@@ -54,6 +78,11 @@ function get_json() {
 }
 
 function json_subscribe() {
+  # continuously print bspc config state in json format
+  # print only if the value changes and poll with an interval
+  # in order to not max out a core.
+  # param $1: sleep interval
+  # param $2: the command fragment to evaluate
   local old new
   old="$(get_json)"
   while sleep "$1"; do
@@ -66,6 +95,8 @@ function json_subscribe() {
 }
 
 function json_subscribe_preset() {
+  # similar to json_subscribe but this one will print the current
+  # bspc config state before going into the loop
   local old new
   old="$(get_json)"
   echo "$old"
@@ -79,6 +110,9 @@ function json_subscribe_preset() {
 }
 
 function set_env() {
+  # similar to get_json but this does not use a json format
+  # just print the values line by line
+  # param $1: variable prefix eg: $1=y -> y_top_padding
   local config_value
   for variable in "${wm_config[@]}"; do
     config_value="$(bspc config "$variable")"
@@ -87,6 +121,10 @@ function set_env() {
 }
 
 function raw_subscribe() {
+  # similar to json_subscribe but prints the values line by line
+  # see: set_env
+  # param $1: sleep interval
+  # param $2: arbitary command to run
   local old_current new_current old_format
   set_env 'old_'
   while sleep "$1"; do
@@ -102,20 +140,21 @@ function raw_subscribe() {
 }
 
 function notify_choice() {
+  # eh.. I don't need to explain this
   notify-send -a layouts -i custom-sliders LayoutCTL "<span foreground='$yellow'>$1</span> has been set from <span font_weight='bold' foreground='$red'>$2</span> to <span font_weight='bold' foreground='$green'>$3</span>"
 }
 
-function echo_choice () {
+function echo_choice() {
   echo "$1 $2 $3"
 }
 
 case "$1" in
-  --json|-j) get_json;;
-  --sub-json|-sj) json_subscribe "$2" 'get_json';;
-  --sub|-s) json_subscribe "${@:2}";;
-  --sub-short|-ss) json_subscribe_preset "${@:2}";;
-  --raw-sub|-rs) raw_subscribe "$2" 'echo_choice';;
-  *) raw_subscribe 0.1 'notify_choice';;
+  --json | -j) get_json ;;
+  --sub-json | -sj) json_subscribe "$2" 'get_json' ;;
+  --sub | -s) json_subscribe "${@:2}" ;;
+  --sub-short | -ss) json_subscribe_preset "${@:2}" ;;
+  --raw-sub | -rs) raw_subscribe "$2" 'echo_choice' ;;
+  *) raw_subscribe 0.1 'notify_choice' ;;
 esac
 
 # vim:filetype=sh
