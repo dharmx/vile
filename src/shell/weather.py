748ccc59ecb2f76b2b50beb2b7440a8c63e8acf8
dharmx <dharmx.dev@gmail.com>
2022-07-27 01:46:59 +0530

    docs

---
+++ src/shell/weather.py
@@ -0,0 +1,12 @@
+import sys
+from datetime import datetime
+from json import dumps, loads
+from os import getenv, listdir, mkdir, system
+from os.path import isdir
+
+from dotenv import load_dotenv
+from requests import get
+
+
+
+# vim:filetype=python
